199cc5cfb5d8fd23a5b724dd7a18761688b9cbcc
tuannguyen <nguyennanhtuan0799@gmail.com>
2025-03-27 21:49:58 +0700

    first commit

---
+++ assets/Images.js
@@ -0,0 +1,11 @@
+export default {
+  background: require('./img/background.png'),
+  sprites: require('./img/sprites.png'),
+  healthIcon: require('./img/icon_health.png'),
+  pauseIcon: require('./img/icon_pause.png'),
+  timeIcon: require('./img/icon_time.png'),
+  scoreIcon: require('./img/icon_score.png'),
+  restartIcon: require('./img/icon_restart.png'),
+  playIcon: require('./img/icon_play.png'),
+  hummerIcon: require('./img/icon_hummer.png'),
+};
